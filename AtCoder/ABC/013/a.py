0ddf1e223dfdbc5223acfef462ab7a8a931b01f2
yuta <namtok.dev@gmail.com>
2020-12-10 23:19:07 +0900

    Create a.py

---
+++ AtCoder/ABC/013/a.py
@@ -0,0 +1,20 @@
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    count = 0
+    for i in range(N):
+        while i + 1 < N and A[i] == A[i + 1]:  # これはA[0]から連続で同じ値が続くときだけ動く
+            i += 1
+        if i + 1 < N and A[i] < A[i + 1]:
+            while i + 1 < N and A[i] <= A[i + 1]:
+                i += 1
+        elif i + 1 < N and A[i] > A[i + 1]:
+            while i + 1 < N and A[i] >= A[i + 1]:
+                i += 1
+        count += 1
+    print(count)
+
+
+main()
+
+# 参考：https://drken1215.hatenablog.com/entry/2019/03/28/014100
