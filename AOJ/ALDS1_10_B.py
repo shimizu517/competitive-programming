2369ed078070a880f9434bc42c34e54100e03750
yuta <yuta.gsfire@gmail.com>
2021-01-14 23:31:29 +0900

    comment

--- AOJ/ALDS1_10_B.py
+++ AOJ/ALDS1_10_B.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 N = int(input())
 p = [[]]
 for i in range(N):
@@ -13,10 +16,13 @@ def matrix_chain_order(p):
         for i in range(1, n + 2 - l):  # 始まりの地点は1~n+1-lまで
             j = i + l - 1  # 終わりの地点はi+1~i+l-1まで。i<jとなるように。
             m[i][j] = float("inf")
+            # 行列積A(i)..A(j)を部分行列積A(i)..A(k)とA(k+1)..A(j)に分けてそれぞれの最小乗算回数と各部分行列積の結果の行列を乗算する
+            # ただし、i<=k<j
             for k in range(i, j):
                 q = m[i][k] + m[k + 1][j] + p[i][0] * p[k][1] * p[j][1]
                 if q < m[i][j]:
                     m[i][j] = q
+    pprint(m)
     return m[1][n]
 
 
