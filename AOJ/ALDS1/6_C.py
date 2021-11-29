81560888ce7a9dbfe79db05c5152fdf4a8eaad24
yuta <yuta.gsfire@gmail.com>
2021-11-29 21:38:25 +0900

    quick sort

---
+++ AOJ/ALDS1/6_C.py
@@ -0,0 +1,19 @@
+def partition_str(a, p, r):
+    v = a[r]
+    idx = p - 1
+    for i in range(p, r + 1):
+        if a[i] <= v:
+            idx += 1
+            a[idx], a[i] = a[i], a[idx]
+    return list(map(str, a))
+
+
+def main():
+    _ = input()
+    a = list(map(int, input().split(' ')))
+    r = len(a) - 1
+    result = partition_str(a, 0, r)
+    print(' '.join(result))
+
+
+main()
