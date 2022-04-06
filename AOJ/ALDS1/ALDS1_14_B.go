b2ac36a36951396c618cd29f0b4b4aaecc90864b
yuta <namtok.dev@gmail.com>
2022-04-06 20:40:22 +0900

    alds1_14_b.go

---
+++ AOJ/ALDS1/ALDS1_14_B.go
@@ -0,0 +1,42 @@
+// aoj KSHIBATA101 さんの解答。速いので参考。
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const B uint64 = 100000007
+
+func main() {
+	var t, p string
+	fmt.Scan(&t)
+	fmt.Scan(&p)
+
+	ts, ps := []rune(t), []rune(p)
+	n, m := len(ts), len(ps)
+	if n < m {
+		return
+	}
+
+	var th, ph, bm uint64 = 0, 0, 1
+	for i := 0; i < m; i++ {
+		th = uint64(ts[i]) + th*B
+		ph = uint64(ps[i]) + ph*B
+		bm = bm * B
+	}
+	wr := bufio.NewWriter(os.Stdout)
+	if th == ph {
+		wr.WriteString("0\n")
+	}
+	for i := 1; i <= n-m; i++ {
+		th = th*B + uint64(ts[i+m-1]) - uint64(ts[i-1])*bm
+		if th == ph {
+			wr.WriteString(strconv.Itoa(i) + "\n")
+		}
+	}
+	wr.Flush()
+}
--- AOJ/ALDS1/ALDS1_14_B_rabin_karp.go
+++ AOJ/ALDS1/ALDS1_14_B_rabin_karp.go
@@ -1,3 +1,4 @@
+// TLE
 package main
 
 import (
